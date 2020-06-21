a476135d361210e377cd844a8e0f305990ee85db
Agustin <0j30j2@gmail.com>
2020-06-21 17:50:18 -0300

    Cambios minimos

--- app.js
+++ app.js
@@ -129,10 +129,10 @@ function join() {
 }
 
 function createRoom(fn) {
-    var name = 'room-' + numbRoom;
-    var numbRoom2 = numbRoom;
+    let name = 'room-' + numbRoom;// numbRoom es el numero inicial de las room
+    let numbRoom2 = numbRoom;
     numbRoom++;
-    rooms.push({
+    rooms.push({// incerto el nuevo romm en la variable general
         'name': name,
         'players': [],
         'timeMin': 2,
@@ -142,7 +142,7 @@ function createRoom(fn) {
         'votes': 0,
         'finish':false
     });
-    for (var i = 0; i < wait_list.length; i++) {
+    for (let i = 0; i < wait_list.length; i++) {
         rooms[numbRoom2].players.push({
             'id': wait_list[i].id,
             'name': wait_list[i].name,
@@ -155,9 +155,9 @@ function createRoom(fn) {
             'votes': 0
         });
 
-        for (var q = 0; q < rooms[numbRoom2].players.length; q++) {
+        for (let q = 0; q < rooms[numbRoom2].players.length; q++) {
             if (rooms[numbRoom2].players[q].id == wait_list[i].id) {
-                for (var j = 0; j < size * size; j++) {
+                for (let j = 0; j < size * size; j++) {
                     rooms[numbRoom2].players[q].image[0].pixels.push({ 'r': 255, 'g': 255, 'b': 255 });
                 }
             }
@@ -167,7 +167,7 @@ function createRoom(fn) {
 }
 
 function info() {
-    for (var i = 0; i < wait_list.length; i++) {
+    for (let i = 0; i < wait_list.length; i++) {
         io.to(wait_list[i].id).emit('info',{ 'type':1,'length':wait_list.length,'time':timers});
     }
     if (timers <= 0) {
